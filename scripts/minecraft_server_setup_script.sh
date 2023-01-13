d3d63a0cc6933099baf03b1d6076233819eb418d
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2023-01-13 23:31:14 +0530

    compose and script update

--- scripts/minecraft_server_setup_script.sh
+++ scripts/minecraft_server_setup_script.sh
@@ -3,3 +3,45 @@ mkdir minecraft_server
 cd minecraft_server
 wget https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar
 echo "eula=true" > eula.txt
+
+echo "FROM openjdk:17
+
+WORKDIR /server
+
+COPY . .
+
+EXPOSE 25565/tcp
+
+EXPOSE 25565/udp
+
+EXPOSE 25575/tcp
+
+CMD [\"java\", \"-jar\", \"-Xmn9G\", \"-Xmx18G\", \"server.jar\", \"nogui\"]" > Dockerfile
+
+echo "version: '3.9'
+services:
+  minecraft:
+    image: minecraft
+    container_name: minecraft
+    stdin_open: true
+    restart: unless-stopped
+    tty: true
+    ports:
+      - 25565:25565
+      - 25575:25575
+    volumes:
+      - minecraft_data:/server
+volumes:
+  minecraft_data:
+    driver: local
+    driver_opts:
+      o: bind
+      type: none
+      device: ${HOME}/minecraft_server
+
+networks:
+  proxy:
+    name: proxy" > docker-compose.yml
+
+docker build -t minecraft .
+docker compose up -d
